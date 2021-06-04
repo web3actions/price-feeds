feaea1b34a710d8d9fbe99fb24bee2f551146525
mktcode <kontakt@markus-kottlaender.de>
2021-06-04 10:31:54 +0200

    implemented chainlink eth/usd price feed

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const getPrice = require('./get-price')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const price = await getPrice()
+    core.info(price)
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('int', price.int)
+    core.setOutput('price', price.price)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
